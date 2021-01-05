ab1325974866b8b47444ef0cfd73ab37c5bf2b30
tillschmidt <schmidt.till.mail@gmail.com>
2021-01-05 21:06:35 +0100

    add LoginForm to StartForm without Firebase and Redux

--- sell-teile/src/components/StartForm/StartForm.tsx
+++ sell-teile/src/components/StartForm/StartForm.tsx
@@ -1,30 +1,64 @@
 
-import React, { useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import Modal from 'react-modal'
-
-function signUp ()  {
-
+import { User } from '../../types'
 
 
 
-}
+function StartForm() {
 
-function SignIn () {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [userCredentials, setuserCredentials] = useState<User>({ name: '', password: 0 })
 
-}
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
 
-function StartForm () {
 
-  const [modalIsOpen, setmodalIsOpen] = useState(false)
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setuserCredentials(prevState => ({ ...prevState, [name]: value }))
+
+  }
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+    const { name, value } = event.target
+
+    setuserCredentials(previousState => ({ ...previousState, [name]: value }))
+
+  }
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(userCredentials)
+  
+
+
+  }
 
   return (
 
     <div className="start_form_container">
       <div className="start_form_button_container">
-        <button className="sign_in_button">Log In</button>
+        <button
+          onClick={() => setModalIsOpen(true)}
+          className="sign_in_button">Log In
+         </button>
         <Modal
-        isOpen= {modalIsOpen}
-        ></Modal>
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel={'Login Daten eingeben!'}
+        >
+          <form action="submit">
+            <label htmlFor="benutzer_name">Benutzer Name</label>
+            <input name='name' type="text" onChange={handleNameChange} />
+            <label htmlFor="password">Passwort</label>
+            <input name='password' type="text" onChange={handlePasswordChange} />
+            <button type="submit" onClick={handleSubmit}>Log in</button>
+          </form>
+
+        </Modal>
       </div>
     </div>
   )
