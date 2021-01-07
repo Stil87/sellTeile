a1c254be7b0963b7d853a7ae1ccc818fe92fd5f4
tillschmidt <schmidt.till.mail@gmail.com>
2021-01-07 13:55:05 +0100

    add CircularProgress and material-ui/core package

--- sell-teile/src/components/StartForm/StartForm.tsx
+++ sell-teile/src/components/StartForm/StartForm.tsx
@@ -2,14 +2,16 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { User } from '../../types'
 import { firebaseLogin } from '../../utils/firebaseFunctions'
+import { CircularProgress } from '@material-ui/core'
 
 
 
-function StartForm():JSX.Element {
+function StartForm(): JSX.Element {
 
   const [userCredentials, setuserCredentials] = useState<User>({ email: '', password: 0 })
+  const [progressBar, setprogressBar] = useState(false)
 
- 
+
 
 
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -27,11 +29,13 @@ function StartForm():JSX.Element {
   }
 
   const handleSubmit = async (event: FormEvent) => {
+    setprogressBar(true)
     event.preventDefault();
     console.log(userCredentials)
     const user = await firebaseLogin(userCredentials)
- 
-   
+    setprogressBar(false)
+
+
 
 
 
@@ -40,19 +44,19 @@ function StartForm():JSX.Element {
   return (
 
     <div className="start_form_container">
-      
-       
-       
-          <form action="submit">
-            <label htmlFor="email">Email</label>
-            <input name='email' type="email" onChange={handleNameChange} />
-            <label htmlFor="password">Passwort</label>
-            <input name='password' type="text" onChange={handlePasswordChange} />
-            <button type="submit" onClick={handleSubmit}>Log in</button>
-          </form>
 
-       
-      </div>
+      {progressBar ? <CircularProgress /> : null}
+
+      <form action="submit">
+        <label htmlFor="email">Email</label>
+        <input name='email' type="email" onChange={handleNameChange} />
+        <label htmlFor="password">Passwort</label>
+        <input name='password' type="text" onChange={handlePasswordChange} />
+        <button type="submit" onClick={handleSubmit}>Log in</button>
+      </form>
+
+
+    </div>
   )
 
 }
